0ba94d1c4d1213f984e6714b7132e642b030be64
Yas Marano <yaspachano@gmail.com>
2026-09-08 21:12:09 -0300

    Agregada la funcionalidad para llenar el contenido de las tarjetas en addWorkCard() se actualiza el titulo, description y se establece la img

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-function addWorkCard() {
+function addWorkCard(params = {}) {
   const template = document.querySelector("#portfolio-card-template");
   const container = document.querySelector(".portfolio-content");
-  var clone = document.importNode(template.content, true);
+
+  template.content.querySelector(".portfolio-card-title").textContent =
+    params.title;
+  template.content.querySelector(".portfolio-card-description").textContent =
+    params.description;
+  template.content.querySelector(".portfolio-card-img").src = params.image;
+  const clone = document.importNode(template.content, true);
   container.appendChild(clone);
 }
 
 function main() {
-  addWorkCard();
-  addWorkCard();
-  addWorkCard();
+  addWorkCard({
+    title: "Project 1",
+    description: "Description of Project 1",
+    image: "path/to/image1.jpg",
+  });
+  addWorkCard({
+    title: "Project 2",
+    description: "Description of Project 1",
+    image: "path/to/image1.jpg",
+  });
+  addWorkCard({
+    title: "Project 3",
+    description: "Description of Project 1",
+    image: "path/to/image1.jpg",
+  });
 }
 
 main();
