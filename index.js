f3841b819b34c5bb22f1caa4c3c9f1cba4dc4d26
Yas Marano <yaspachano@gmail.com>
2026-09-09 10:29:27 -0300

    Puse fotos bonitas de Cyberpunk Edgerunners en las tarjetas con Javascript

--- index.js
+++ index.js
@@ -15,17 +15,20 @@ function main() {
   addWorkCard({
     title: "Project 1",
     description: "Description of Project 1",
-    image: "path/to/image1.jpg",
+    image:
+      "https://i.pinimg.com/736x/49/0e/af/490eafcde12cf7634ff8cf33241a318f.jpg",
   });
   addWorkCard({
     title: "Project 2",
-    description: "Description of Project 1",
-    image: "path/to/image1.jpg",
+    description: "Description of Project 2",
+    image:
+      "https://i.pinimg.com/736x/b4/39/83/b439835f0d72eb89cf65b881298e4d27.jpg",
   });
   addWorkCard({
     title: "Project 3",
-    description: "Description of Project 1",
-    image: "path/to/image1.jpg",
+    description: "Description of Project 3",
+    image:
+      "https://i.pinimg.com/736x/b6/14/4f/b6144f3d3dc2d3f353f5c6972b90b64b.jpg",
   });
 }
 
